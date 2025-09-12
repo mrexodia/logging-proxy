6418c61267f4ecd93f1a541cec67dc0deac7ad30
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-09-12 16:48:13 +0200

    Create standalone stream-test.sh

--- tests/stream-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -X POST http://localhost:5601/lmstudio/v1/chat/completions \
-  -H "Authorization: Bearer YOUR_API_KEY" \
-  -H "Content-Type: application/json" \
-  -d @stream-test.json \
-  --no-buffer
